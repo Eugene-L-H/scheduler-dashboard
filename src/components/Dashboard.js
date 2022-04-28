74c18e9e2b03e8ec4f7d6b2900fd4b99d0bfb0da
Eugene <eugenehelland@mail.com>
2022-04-28 16:46:57 -0600

    Add Loading component.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,25 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import Loading from './Loading';
 
-import classnames from "classnames";
+import classnames from 'classnames';
 
 class Dashboard extends Component {
+  state = { loading: false };
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames('dashboard');
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
 
     return <main className={dashboardClasses} />;
   }
 }
+
+// The same but without classes:
+// function Dashboard(props) {
+//   const dashboardClasses = classnames("dashboard");
+//   return <main className={dashboardClasses} />;
+// }
 
 export default Dashboard;
